c93f154bdc382f2469b63c1420f525bff189826b
komal837-gif <utagekomal57@gmail.com>
2026-01-01 17:29:44 +0530

    remove functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './shared/components/todo/todo.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import { GetConfirmComponent } from './get-confirm/get-confirm.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    GetConfirmComponent
   ],
   imports: [
     BrowserModule,
     MatIconModule,
     MatButtonModule,
     BrowserAnimationsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatDialogModule
 
   ],
   providers: [],
--- src/app/shared/components/todo/todo.component.ts
+++ src/app/shared/components/todo/todo.component.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { todoArr } from 'src/app/const/todo';
 import { Itodo } from 'src/app/model/todo';
 import { SnackbarService } from '../../services/snackbar.service';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { GetConfirmComponent } from 'src/app/get-confirm/get-confirm.component';
 
 @Component({
   selector: 'app-todo',
@@ -11,7 +13,9 @@ import { SnackbarService } from '../../services/snackbar.service';
 export class TodoComponent implements OnInit {
   @ViewChild('todoInput') todoInput!:ElementRef
  todoData:Array<Itodo> = todoArr
-  constructor(private snack:SnackbarService) { }
+  constructor(private snack:SnackbarService,
+    private matDialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
   }
@@ -28,7 +32,21 @@ export class TodoComponent implements OnInit {
     this.todoInput.nativeElement.value = '';
     }
     
-    
+
+    onTodoRemove(id:string){
+     let matConfig = new MatDialogConfig()
+     matConfig.disableClose = true
+     matConfig.width = "500px"
+     let matDialogRef = this.matDialog.open(GetConfirmComponent,matConfig)
+     matDialogRef.afterClosed()
+      .subscribe(res=>{
+       if(res){
+        let getIndex = this.todoData.findIndex(todo=>todo.id === id)
+        this.todoData.splice(getIndex,1)
+        this.snack.showSuccess(`The todo item with id ${id} is removed successfully!!!`)
+       }
+      })
+    }
 
   }
 
